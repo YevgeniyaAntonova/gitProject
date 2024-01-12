d7344792373a076dd3ddd1689a785cf28ec76feb
Yevgeniya Antonova <epozdeyeva@gmail.com>
2024-01-13 00:16:24 +0600

    homewor module 2 task2

--- src/module2/task2/Colony.java
+++ src/module2/task2/Colony.java
@@ -1,45 +1,56 @@
 package module2.task2;
 
+import java.util.ArrayList;
+
 public class Colony {
     String name;
     int smetPrice;
     int apCount;
-    Apartment[] aparts;
-    public Colony(){}
-    public Colony(String name, int smetPrice, int apCount){
-        this.name = name;
-        this. smetPrice = smetPrice;
-        this.apCount = apCount;
+    Apartment apart;
+
+    public Colony() {
     }
-    public Colony(String name, int smetPrice, int apCount, Apartment[] aparts){
+
+    public Colony(String name, int smetPrice, int apCount, ArrayList<Apartment> aparts) {
         this.name = name;
         this.smetPrice = smetPrice;
         this.apCount = apCount;
-        this.aparts = aparts;}
+        this.apart = apart;
+    }
 
-        public int getApCount(){
-        return apCount;
-        }
-    public int priceAp(TypeApartment type, int smetPrice, int smet){
+    public Apartment getApart() {
+        return apart;
+    }
+
+    public void setApart(Apartment apart) {
+        this.apart = apart;
+    }
+
+
+    public int priceAp(TypeApartment type, int smetPrice, int smet) {
         int price = smetPrice * smet;
-        switch(type){
-            case Carbonpanel -> {
-                price = (int) (0.8 * price);
-                break;
+        //for (int i = 0; i < apCount; i++) {
+
+            switch (type) {
+                case Carbonpanel -> {
+                    price = (int) (0.8 * price);
+                    break;
+                }
+                case Cyberbricks -> {
+                    price = (int) (1.5 * price);
+                    break;
+                }
+                case Nanolite -> {
+                    price = price;
+                    break;
+                }
             }
-            case Cyberbricks -> {
-                price = (int) (1.5 * price);
-                break;
-            }
-            case Nanolite -> {
-                price = price;
-                break;
-            }
-        }
-       return price;
-    }
+    //}
+            return price;
+
 
     }
+}
 
 
 
--- src/module2/task2/RunnerColony.java
+++ src/module2/task2/RunnerColony.java
@@ -1,6 +1,8 @@
 package module2.task2;
 
+import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class RunnerColony {
@@ -9,31 +11,26 @@ public class RunnerColony {
     public static void main(String[] args) {
         System.out.println("Count of Colony:");
         int n = scanner.nextInt();
-        int minPrice =0;
-        int price;
+        int priceAp;
+        int minPrice = 0;
 
         ArrayList<Colony> colonies = new ArrayList<Colony>();
-
         for (int i = 0; i < n; i++) {
-            colonies.add(new Colony(scanner.next(), scanner.nextInt(), scanner.nextInt()));
-            System.out.println(colonies.get(i).name);
             ArrayList<Apartment> aparts = new ArrayList<Apartment>();
+            colonies.add(new Colony(scanner.next(), scanner.nextInt(), scanner.nextInt(), aparts));
+
             for (int j = 0; j < colonies.get(i).apCount; j++) {
                 aparts.add(new Apartment(scanner.next(), scanner.nextInt(), scanner.next()));
-                System.out.println(aparts.get(j).title + " " + aparts.get(j).smet + " " + aparts.get(j).type);
+                System.out.printf("%s %d %s %n", aparts.get(j).title, aparts.get(j).smet, aparts.get(j).type);
+                priceAp = colonies.get(i).priceAp(aparts.get(j).type, colonies.get(i).smetPrice, aparts.get(j).smet);
+                System.out.printf("%s - %d %n", aparts.get(j).title, priceAp);
 
-            price = colonies.get(i).priceAp(aparts.get(j).type,colonies.get(i).smetPrice, aparts.get(j).smet);
-            minPrice = colonies.get(0).priceAp(aparts.get(0).type,colonies.get(0).smetPrice, aparts.get(0).smet);
-                System.out.println(price);
-                //System.out.println(minPrice);
-                if (price < minPrice){
-                    minPrice = price;
-                    System.out.println(colonies.get(i).name + " " + aparts.get(j).title);
-                } else {minPrice = minPrice;}
+                //найти минимальную стоимость
+
+
             }
-
         }
-
     }
-
 }
+
+
