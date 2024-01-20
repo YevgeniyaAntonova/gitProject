70e894a073d928f00ee7bece34e59292d3ccf3fb
Yevgeniya Antonova <epozdeyeva@gmail.com>
2024-01-20 23:28:28 +0600

    homework module2/task4

---
+++ src/module2/task4/TestPoint3D.java
@@ -0,0 +1,8 @@
+package module2.task4;
+
+public class TestPoint3D {
+    public static void main(String[] args) {
+        Point3D point = new Point3D(3, 6, 5);
+        System.out.println(point.toString());
+    }
+}
