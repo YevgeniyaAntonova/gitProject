d87c9d78079d3577f521d86ccecaf6972acd370b
Yevgeniya Antonova <epozdeyeva@gmail.com>
2024-02-22 19:40:29 +0600

    ArrayList tasks ex2

---
+++ src/module3/ArrayList/ArrayList.java
@@ -0,0 +1,32 @@
+package module3.ArrayList;
+
+import java.util.Scanner;
+
+public class ArrayList {
+    static  Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        java.util.ArrayList<String> names = new java.util.ArrayList<String>();
+       addlist(names);
+        exercise2(names);
+
+    }
+    public static void addlist(java.util.ArrayList<String> names) {
+
+        //Tom,Robert,Alice,Kate,Sam
+        names.add("Tom");
+        names.add("Robert");
+        names.add("Alice");
+        names.add("Kate");
+        names.add("Sam");
+        System.out.println(names);
+    }
+    public static void exercise2(java.util.ArrayList<String> names){
+        String name;
+        for (int i = 0; i < names.size(); i++){
+            name = names.get(i);
+            System.out.println(name);
+
+        }
+
+    }
+}
